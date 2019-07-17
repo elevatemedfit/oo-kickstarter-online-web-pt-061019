756866e9b069c0479d358e6d5fbee44ee11965cf
Chris Junker <elevatemedfit@gmail.com>
2019-07-17 18:16:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,7 +3,7 @@ class Backer
   attr_accessor
   attr_reader :name,:backed_projects
 
-  def initialize(name,backed_projects)
+  def initialize(name)
     @name = name
     @backed_projects = []
   end
